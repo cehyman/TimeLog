03dbd7e7048f1e31a4a738a7351eaa90e3f3e919
Christopher Hyman <chyman@purdue.edu>
2023-12-02 11:59:17 -0500

    sidebar and timeclock

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -8,12 +8,22 @@ const SidebarMenu: React.FC = () => {
     <nav className={styles.sidebar}>
       <ul className={styles.sidebarNav}>
         <li className={styles.navItem}>
-          <a href="/dashboard">Dashboard</a>
+          <a href="/">Dashboard</a>
         </li>
-        {/* ... other list items ... */}
+        <li className={styles.navItem}>
+          <a href="/timeclock">Time Clock</a>
+        </li>
+        <li className={styles.navItem}>
+          <a href="/reports">Reports</a>
+        </li>
+        <li className={styles.navItem}>
+          <a href="/settings">Settings</a>
+        </li>
+        {/* Add more navigation items as needed */}
       </ul>
     </nav>
   );
 }
 
 export default SidebarMenu;
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,14 +8,28 @@ const Home: React.FC = () => {
   return (
     <div>
       <Head>
-        <title>TimeLog - Home</title>
-        <meta name="description" content="Track your time efficiently with TimeLog" />
+        <title>TimeLog - Dashboard</title>
+        <meta name="description" content="Dashboard for tracking your time efficiently with TimeLog" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1>Welcome to TimeLog</h1>
-        {/* Main content */}
+        <h1>Dashboard</h1>
+        <section className={styles.timeTracker}>
+          <button className={styles.clockInButton}>Clock In</button>
+          <button className={styles.clockOutButton}>Clock Out</button>
+          {/* Additional elements for clocking in and out */}
+        </section>
+
+        <section className={styles.summary}>
+          <h2>Today's Summary</h2>
+          {/* Display summary of today's logged time */}
+        </section>
+
+        <section className={styles.recentActivity}>
+          <h2>Recent Activity</h2>
+          {/* List recent time logs */}
+        </section>
       </main>
 
       <footer className={styles.footer}>
