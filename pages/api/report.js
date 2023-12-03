ad9fe7d2ffd26b1d720f6c353e004881ebdcbd66
chyman <chyman@purdue.edu>
2023-12-02 23:55:38 -0500

    report functionality

--- pages/api/report.js
+++ pages/api/report.js
@@ -1,30 +1,49 @@
-// pages/api/report.js
-import db from '../../lib/db'; // Adjust the path to your db.js file
+import db from '../../lib/db';
+import PDFDocument from 'pdfkit';
+import { writeFile, mkdir } from 'fs/promises';
+import { join } from 'path';
 
 export default async (req, res) => {
-  if (req.method === "GET") {
-    // New logic for generating report based on date range
+  if (req.method === 'GET') {
     const { startDate, endDate } = req.query;
+    const reportPath = join(process.cwd(), 'lib', 'reports');
+    const reportFilename = `report_${startDate}_to_${endDate}.pdf`;
+    const reportFullPath = join(reportPath, reportFilename);
 
     try {
+      // Ensure the reports directory exists
+      await mkdir(reportPath, { recursive: true });
+
       const [report] = await db.query(
-        "SELECT * FROM time_logs WHERE DATE(clock_in) BETWEEN ? AND ? ORDER BY clock_in ASC",
+        'SELECT * FROM time_logs WHERE DATE(clock_in) BETWEEN ? AND ? ORDER BY clock_in ASC',
         [startDate, endDate]
       );
-      res.status(200).json(report);
+
+      // Create a PDF document
+      const doc = new PDFDocument();
+      let buffers = [];
+      doc.on('data', buffers.push.bind(buffers));
+      doc.on('end', async () => {
+        let pdfData = Buffer.concat(buffers);
+        await writeFile(reportFullPath, pdfData);
+        res.status(200).json({ message: 'Report generated successfully', filename: reportFilename });
+      });
+
+      // Add content to PDF
+      doc.fontSize(12).text(`Time Log Report from ${startDate} to ${endDate}`, { underline: true }).moveDown();
+      report.forEach(log => {
+        doc.text(`User ID: ${log.user_id} - Clock In: ${log.clock_in} - Clock Out: ${log.clock_out}`).moveDown();
+      });
+
+      // Finalize PDF file
+      doc.end();
+
     } catch (error) {
-      console.error("Error fetching report:", error);
-      res
-        .status(500)
-        .json({ message: "An error occurred while generating the report" });
+      console.error('Error generating report:', error);
+      res.status(500).json({ message: 'An error occurred while generating the report' });
     }
   } else {
-    // Handling other types of requests or missing parameters
-    res.setHeader("Allow", ["POST", "GET"]);
-    res
-      .status(405)
-      .json({
-        message: `Method ${req.method} Not Allowed or missing parameters`,
-      });
+    res.setHeader('Allow', ['GET']);
+    res.status(405).json({ message: `Method ${req.method} Not Allowed` });
   }
 };
--- pages/reports.js
+++ pages/reports.js
@@ -1,41 +1,46 @@
-import React, { useState } from 'react';
-import styles from '../styles/reports.module.css';
+import React, { useState, useEffect } from "react";
+import styles from "../styles/reports.module.css";
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+
+// Import styles
+import "@react-pdf-viewer/core/lib/styles/index.css";
 
 const Reports = () => {
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
   const [reportData, setReportData] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    return () => {
+      if (reportData) {
+        window.URL.revokeObjectURL(reportData);
+      }
+    };
+  }, [reportData]);
 
   const fetchReportData = async () => {
     setLoading(true);
+    setError("");
     try {
-      const response = await fetch(`/api/report?startDate=${startDate}&endDate=${endDate}`);
+      const response = await fetch(
+        `/api/report?startDate=${startDate}&endDate=${endDate}`
+      );
       if (response.ok) {
-        const data = await response.json();
-        setReportData(data);
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(blob);
+        setReportData(url);
       } else {
-        console.error('Failed to fetch report data');
+        console.error("Failed to fetch report data");
+        setError("Failed to fetch report data");
       }
     } catch (error) {
-      console.error('Error fetching report data:', error);
+      console.error("Error fetching report data:", error);
+      setError("Error fetching report data");
     } finally {
       setLoading(false);
     }
-  };
-
-  const downloadReport = () => {
-    const fileName = `report_${startDate}_to_${endDate}.json`;
-    const json = JSON.stringify(reportData, null, 2);
-    const blob = new Blob([json], { type: 'application/json' });
-    const href = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.href = href;
-    link.download = fileName;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    URL.revokeObjectURL(href);
   };
 
   return (
@@ -59,20 +64,20 @@ const Reports = () => {
               onChange={(e) => setEndDate(e.target.value)}
             />
           </label>
-          <button onClick={fetchReportData} disabled={!startDate || !endDate || loading}>
+          <button
+            onClick={fetchReportData}
+            disabled={!startDate || !endDate || loading}
+          >
             Generate Report
           </button>
         </div>
         {loading && <p>Loading report...</p>}
+        {error && <p className={styles.error}>{error}</p>}
         {reportData && (
           <div>
-            <div className={styles.reportResults}>
-              {/* Render your report data here */}
-              <pre>{JSON.stringify(reportData, null, 2)}</pre>
-            </div>
-            <button onClick={downloadReport} className={styles.downloadButton}>
-              Download Report
-            </button>
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.10.377/build/pdf.worker.min.js">
+              <Viewer fileUrl={reportData} />
+            </Worker>
           </div>
         )}
       </div>
